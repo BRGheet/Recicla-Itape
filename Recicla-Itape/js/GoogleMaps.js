e9714c4aedd090ea4f2a5e0e6360041abece533a
Miqueias08 <miqueiasfernando@gmail.com>
2017-08-18 14:20:22 -0300

    Mapa Adicionado

---
+++ Recicla-Itape/js/GoogleMaps.js
@@ -0,0 +1,21 @@
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+});
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+  var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+  var mapOptions = {
+    zoom: 8,
+    center: myLatlng,
+    mapTypeId: 'roadmap'
+  };
+    var map = new google.maps.Map(document.getElementById('map'),
+    mapOptions);
+}
+
+
