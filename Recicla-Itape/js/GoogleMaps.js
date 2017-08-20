9032c7d5b1a0b39bb54c683a914baa3bb62d1ed4
Miqueias08 <miqueiasfernando@gmail.com>
2017-08-20 00:51:40 -0300

    Upadtes

--- Recicla-Itape/js/GoogleMaps.js
+++ Recicla-Itape/js/GoogleMaps.js
@@ -17,5 +17,14 @@ function initMap() {
     var map = new google.maps.Map(document.getElementById('map'),
     mapOptions);
 }
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
 
 
