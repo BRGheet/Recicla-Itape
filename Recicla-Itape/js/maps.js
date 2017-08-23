89a88cfc4f1c9c19cc1c26377943a90779c60736
Miqueias08 <miqueiasfernando@gmail.com>
2017-08-23 12:33:44 -0300

    Melhorias QueroReciclar

--- Recicla-Itape/js/maps.js
+++ Recicla-Itape/js/maps.js
@@ -3,8 +3,12 @@
         map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -23.588225, lng: -48.040106},
           zoom: 13,
+          /*Minimo zoom limite*/
+          minZoom:13,
           zoomControl: false,
           scaleControl: false,
           streetViewControl: false,
+          /*Desabilita ctrl para zoon*/
+          gestureHandling: 'greedy'
         });
       }
