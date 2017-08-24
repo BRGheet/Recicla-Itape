d96e79111569e07ef6deb12463cb1469347070ec
Miqueias08 <miqueiasfernando@gmail.com>
2017-08-24 16:54:40 -0300

    Tela Usuario Logado

--- Recicla-Itape/user.php
+++ Recicla-Itape/user.php
@@ -57,10 +57,16 @@
     <a href="#"><i class="fa fa-comment" aria-hidden="true"></i>Dicas</a>
     <a href="#"><i class="fa fa-question-circle" aria-hidden="true"></i>Sobre</a>
   </nav>
+
   <!-- Pontos Usuario -->
-  <div class="container">
-  	
-  </div>
+    <div class="container-fluid margin">
+      <div class="pontos">
+        <div class="col-md-2 progress">
+          <canvas id="my_canvas" width="70" height="70"></canvas>
+        </div>
+      </div>
+      
+    </div>
 
 
 
@@ -72,7 +78,32 @@
 
 
 
-
+    <script>
+      var ctx = document.getElementById('my_canvas').getContext('2d');
+      var al = 0;
+      var start = 4.72;
+      var cw = ctx.canvas.width;
+      var ch = ctx.canvas.height; 
+      var diff;
+      function progressSim(){
+        diff = ((al / 100) * Math.PI*2*10).toFixed(2);
+        ctx.clearRect(0, 0, cw, ch);
+        ctx.lineWidth = 10;
+        ctx.fillStyle = '#D3D3D3';
+        ctx.strokeStyle = "#D3D3D3";
+        ctx.textAlign = 'center';
+        ctx.fillText(al+'%', cw*.5, ch*.5+2, cw);
+        ctx.beginPath();
+        ctx.arc(35, 35, 30, start, diff/10+start, false);
+        ctx.stroke();
+        if(al >= 100){
+          clearTimeout(sim);
+      // Add scripting here that will run when progress completes
+    }
+    al++;
+  }
+  var sim = setInterval(progressSim, 50);
+    </script>
     <!-- Script NavBar -->
     <script type="text/javascript" src="js/Navbar.js"></script>
     <!-- <script src="https://use.fontawesome.com/eb29782670.js"></script> -->
