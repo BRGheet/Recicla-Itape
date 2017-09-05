3f84864e062e52be65f7fc9e36547128da947af0
Miqueias08 <miqueiasfernando@gmail.com>
2017-09-05 14:14:04 -0300

    Melhorias nas rotas

---
+++ Recicla-Itape/recicla/app/Http/Controllers/site/CadastroController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\site;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CadastroController extends Controller
+{
+    public function cadastro(){
+    	return('rt');
+    }
+}
--- Recicla-Itape/recicla/routes/web.php
+++ Recicla-Itape/recicla/routes/web.php
@@ -1,11 +1,12 @@
 <?php
-Route::resource('/entrar/','CadastroLoginController');
-
 Route::get('home', 'siteController@index');
 Route::get('/', 'siteController@index');
 Route::get('reciclar', 'siteController@queroReciclar');
 Route::get('cooperativas', 'siteController@Cooperativas');
 
+/*Login e Cadastro*/
+Route::group(['namespace'=>'site'],function(){
+	Route::get('cadastro/entrar','CadastroController@cadastro');
+});
 
-/*Route::get('entrar', 'CadastroLoginController@entrar');
-Route::get('entrar/cadastro', 'CadastroLoginController@PostCadastro');*/
+Route::resource('/entrar','CadastroLoginController');
