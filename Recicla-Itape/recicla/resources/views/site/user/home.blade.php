adf69942185ae677c73a71dd6ef72e76988b5b62
Miqueias08 <miqueiasfernando@gmail.com>
2017-10-02 15:13:36 -0300

    Tela Usuario Melhorada

--- Recicla-Itape/recicla/resources/views/site/user/home.blade.php
+++ Recicla-Itape/recicla/resources/views/site/user/home.blade.php
@@ -12,42 +12,70 @@
 		</div>
 	</div>
 </div>
-<div class="container-fluid UserContent">
-	<div class="container HistoricBox">
-		<div class="col-md-7 Historic">
-			<p>Historico de Entregas</p>
-			<table class="table">
-				<thead>
-					<tr>
-						<th>Data</th>
-						<th>Horario</th>
-						<th>Local</th>
-						<th>Material</th>
-						<th>Pontos</th>
-					</tr>
-				</thead>
 
-				<tbody>
-					<tr>
-						<td>08/09/2017</td>
-						<td>15:12</td>
-						<td>Cooperita</td>
-						<td>Plastico</td>
-						<td>5</td>
-					</tr>
-					<tr>
-						<td>08/09/2017</td>
-						<td>15:12</td>
-						<td>Cooperita</td>
-						<td>Plastico</td>
-						<td>5</td>
-					</tr>
-				</tbody>
-			</table>
+<div class="container-fluid UserContent">
+	<div class="row">
+		<!-- Recompensas -->
+		<div class="col-md-3  rewards">
+			<h1>Recompensas</h1>
+			<div class="imageRewards col-md-12">
+				<div class="ImageBene">
+				</div>
+				<div class="rewardsText col-md-12">
+					<p>Frete grátis em compras a partir de R$ 110.</p> 
+					<div class="col-md-12 InfoReward">
+						<a href="">mais informações</a>
+					</div>
+				</div>
+			</div>
+
 		</div>
+		<!-- Historico de pontos -->
+		<div class="col-md-6  historicMain">
+			<div class="historic">
+				<table class="table">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Data</th>
+							<th>Hora</th>
+							<th>Cooperativa</th>
+							<th>Material</th>
+							<th>Pontos</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th scope="row">1</th>
+							<td>08/02/2017</td>
+							<td>13:49</td>
+							<td>Cooperita</td>
+							<td>Plastico</td>
+							<td>5 Pontos</td>
+						</tr>
+						<tr>
+							<th scope="row">2</th>
+							<td>09/02/2017</td>
+							<td>14:19</td>
+							<td>Cooperita</td>
+							<td>Metal</td>
+							<td>15 Pontos</td>
+						</tr>
+						<tr>
+							<th scope="row">3</th>
+							<td>09/02/2017</td>
+							<td>11:32</td>
+							<td>Cooperita</td>
+							<td>Orgânico</td>
+							<td>2 Pontos</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+		<div class="col-md-3"></div>
 	</div>
 </div>
-
 <!-- Css Para background-color User -->
 <style type="text/css">
 body{
