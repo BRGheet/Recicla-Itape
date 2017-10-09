8d448cb7f7a40979e4979bdfd2395697d2661d3d
BRGheet <brgheet@gmail.com>
2017-10-09 16:17:15 -0300

    Update #4
    - Estilo dos botões de filtro

--- Recicla-Itape/recicla/resources/views/site/main/queroReciclar.blade.php
+++ Recicla-Itape/recicla/resources/views/site/main/queroReciclar.blade.php
@@ -5,17 +5,30 @@
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-        <h4 class="modal-title" id="ModalLabel">Modal title</h4>
+        <h4 class="modal-title" id="ModalLabel">Selecione um filtro para a melhor visualização do mapa.</h4>
       </div>
       <div class="modal-body">
-        <a href="/reciclar/f=papel">
-          <div class="col-md-6">
-            <span class="glyphicon glyphicon-file"></span> Papel
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-md-6">
+              <button class="btn btn-green btn-filter" value="/reciclar/f=papel"><span class="glyphicon glyphicon-file"></span> Papéis</button>
+            </div>
+            <div class="col-md-6">
+              <button class="btn btn-green btn-filter" value="/reciclar/f=plastico"><span class="glyphicon glyphicon-cd"></span> Plásticos</button>
+            </div>
           </div>
-        </a>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+          <div class="row">
+            <div class="col-md-6">
+              <button class="btn btn-green btn-filter" value="/reciclar/f=vidro"><span class="glyphicon glyphicon-glass"></span> Vidros</button>
+            </div>
+            <div class="col-md-6">
+              <button class="btn btn-green btn-filter" value="/reciclar/f=metal"><span class="glyphicon glyphicon-oil"></span> Metais</button>
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+        </div>
       </div>
     </div>
   </div>
@@ -31,6 +44,10 @@
 <script type="text/javascript">
     $('.filter').click(function(){
       $('#modal').modal('show');
+    });
+    $('.btn-filter').click(function(){
+      var url = $(this).val();
+      $(location).attr('href',url);
     });
 	  function initMap() {
         var meio = {lat: -23.588225, lng: -48.040106};
@@ -48,17 +65,18 @@
 
         @foreach($markers as $marker)
         	var info{{ $marker->id }} = new google.maps.InfoWindow({
-          		content: "<h1>{{ $marker->name }}</h1>"
+          		content: "<h2>{{ $marker->name }}</h2>"
           		+"<p>{{ $marker->content }}</p>"
-          		+"<br><strong>Endereço:</strong> {{ $marker->address }}<br>"
+          		+"<h5>Endereço</h5> {{ $marker->address }}<br>"
+              +"<h5>A cooperativa coleta os seguintes tipos de lixo</h5> <br>"
           		@if($marker->papel)
-          			+"<strong>Papel</strong> "
+          			+"<span class=\"glyphicon glyphicon-file\"></span><strong> Papel</strong> "
           		@endif
           		@if($marker->plastico)
-          			+" <strong>Plastico</strong> "
+          			+"<span class=\"glyphicon glyphicon-cd\"></span><strong> Plastico</strong> "
           		@endif
           		@if($marker->vidro)
-          			+"<strong>Vidro</strong>"
+          			+"<span class=\"glyphicon glyphicon-glass\"></span><strong> Vidro</strong>"
           		@endif
         	});
        	 	var marker{{ $marker->id }} = new google.maps.Marker({
