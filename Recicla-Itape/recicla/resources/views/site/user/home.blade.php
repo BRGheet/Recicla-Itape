4a0788d29697f2d2c3974f30003fd0a6b4ad4d70
Miqueias08 <miqueiasfernando@gmail.com>
2017-10-20 15:29:20 -0200

    User Updates

--- Recicla-Itape/recicla/resources/views/site/user/home.blade.php
+++ Recicla-Itape/recicla/resources/views/site/user/home.blade.php
@@ -5,13 +5,31 @@
 	<div class="row">
 		<div class="sidebarUser col-md-3">
 			<h2>STATUS DA SUA CONTA</h2>
-			<div class="pontosUser col-md-12">
-				<dl>
+			<div class="col-md-12 userButtons">
+				<dl class="pontosUser">
 					<dt>Atualmente você tem:</dt>
 					<dd><span>0</span><br>
 					Pontos Disponíveis.</dd>
 				</dl>
+				<!-- Botoes conta -->
+				<nav class="menu-user-btn">
+					<ul>
+						<a href="">
+							<li>Minha Conta</li>
+						</a>
+					</ul>
+				</nav>
 			</div>
+
+
+
+
+
+
+
+
+
+
 		</div>
 	</div>
 </div>
