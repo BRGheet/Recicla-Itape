a5b0a7be30ef10e1d2f01f32c8d24f8e9d0b3859
Miqueias08 <miqueiasfernando@gmail.com>
2017-10-20 16:37:28 -0200

    Updates

--- Recicla-Itape/recicla/resources/views/site/user/home.blade.php
+++ Recicla-Itape/recicla/resources/views/site/user/home.blade.php
@@ -32,16 +32,32 @@
 					</ul>
 				</nav>
 			</div>
+		</div>
+		<div class="col-md-9 InformMain">
+			<div class="row">
+				<div class="IformTitle">
+					<h2>CONFIRA AS PROMOÇÕES ATIVAS</h2>
+				</div>
+				<div class="promoActive col-md-12">
+
+
+					<div class="col-sm-6 col-md-4 PromoItensMain">
+						<div class="row">
+							<div class="thumbnail PromoUserThub">
+								<img src="https://www.mcdonalds.com/content/dam/usa/nutrition/items/evm/h-mcdonalds-Double-Quarter-Pounder-with-Cheese-Extra-Value-Meals.png" alt="Imagem não encontrada" >
+								<div class="caption">
+									<div class="col-md-12 MarcaPromo">
+										<img src="https://seeklogo.com/images/M/mcdonald-s-logo-0D95A820B2-seeklogo.com.png" style="height: 50px;">
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
 
 
 
-
-
-
-
-
-
-
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
