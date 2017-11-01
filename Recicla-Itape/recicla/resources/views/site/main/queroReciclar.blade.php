7bb558f78c3df62113bd2b0fbb9171bb0f855e9c
Miqueias08 <miqueiasfernando@gmail.com>
2017-11-01 14:30:02 -0200

    Incluido Rodape no Maps

--- Recicla-Itape/recicla/resources/views/site/main/queroReciclar.blade.php
+++ Recicla-Itape/recicla/resources/views/site/main/queroReciclar.blade.php
@@ -33,12 +33,51 @@
     </div>
   </div>
 </div>
-<div class="container-fluid">
-	<div class="row">
-   <button class="btn btn-blue filter"><strong>Filtro</strong></button>
-   <div class="col-md-12" id="map"></div>
- </div>
+<div class="container-fluid mapgoogle">
+  <button class="btn btn-blue filter">
+    <strong>Filtro</strong>
+  </button>
+  <div class="col-md-12" id="map"></div>
 </div>
+
+<footer class="container-fluid">
+  <div class="container footerContent">
+    <div class="col-md-8 col-md-offset-2">
+      <div class="footerBox col-md-4">
+        <h2>Recicla Itapê</h2>
+        <ul>
+          <li>Home</li>
+          <li>Quero Reciclar</li>
+          <li>Cooperativas</li>
+          <li>Entrar</li>
+        </ul>
+      </div>
+      <div class="footerBox col-md-4">
+        <h2>Redes Sociais</h2>
+        <ul>
+          <li>Facebook</li>
+          <li>Instagram</li>
+          <li>Twitter</li>
+        </ul>
+      </div>
+      <div class="footerBox col-md-4">
+        <h2>Contato</h2>
+        <ul>
+          <li>Reclame Aqui</li>
+          <li>Sugestões</li>
+          <li>Duvidas</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+
+<style type="text/css">
+html, body {
+  height: 100%;
+}
+</style>
+
 @endsection
 @push('scripts')
 <script type="text/javascript">
@@ -60,7 +99,7 @@
       scaleControl: true,
       streetViewControl: false,
       /*Desabilita ctrl para zoon*/
-      gestureHandling: 'greedy'
+      /*gestureHandling: 'greedy'*/
     });
 
     @foreach($markers as $marker)
