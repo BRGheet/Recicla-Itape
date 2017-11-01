759db2c6a778526156933ad58827a9fae52095ef
Miqueias08 <miqueiasfernando@gmail.com>
2017-11-01 16:10:29 -0200

    Admin Update

--- Recicla-Itape/recicla/resources/views/site/template/admin.blade.php
+++ Recicla-Itape/recicla/resources/views/site/template/admin.blade.php
@@ -39,6 +39,51 @@
  </script>
 </head>
 <body>
+   <nav class="navbar navbar-default navbar-green">
+    <div class="container">
+      <div class="navbar-header cor-branco">
+        <!-- <a href="#" class="navbar-brand"><img src="img/logo.png" alt="Logo do Recicla Itapê"></a>-->
+        <button type="button" onclick="openNav()" class="navbar-toggle pull-left">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button> 
+        <a href="/" class="navbar-brand hidden-xs">Recicla Itapê</a>
+        <a href="/" class="navbar-brand hidden-sm hidden-md hidden-lg">Recicla Itapê</a>                
+      </div>
+      <div class="col-md-6 col-md-offset-3 hidden-xs">
+        <ul class="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/reciclar">Quero Reciclar</a></li>
+          <li><a href="/cooperativas">Cooperativas</a></li>
+        </ul>
+      </div>
+      <div class="nav navbar-nav navbar-right">
+        <div class="dropdown hidden-xs">
+          @if (Auth::guest())
+          <a href="{{url('cadastro/login')}}"><button class="btn btn-blue-outline navbar-btn">Entrar</button></a>
+          @else
+          <div class="btn-group">
+            <button type="button" class="btn btn-blue-outline navbar-btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              {{ Auth::user()->name }} <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li><a href="#">Resgatar Vouchers</a></li>
+              <li><a href="/vouchers">Vouchers</a></li>
+              <li role="separator" class="divider"></li>
+              <li><a href="/minha_conta">Minha Conta</a></li>
+              <li><a href="{{ route('logout') }}"  onclick="event.preventDefault();
+              document.getElementById('logout-form').submit();">Sair</a>
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}</form>
+            </li>
+          </ul>
+        </div>
+        @endif
+      </div>
+    </div>
+  </div>
+</nav>
   <nav class="navbar navbar-default navbar-green hidden-lg hidden-md">
     <div class="container">
       <div class="navbar-header cor-branco">
