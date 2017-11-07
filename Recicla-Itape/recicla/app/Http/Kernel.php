b51489857af02caa31281805f31a34f6b5e46d9f
Miqueias08 <miqueiasfernando@gmail.com>
2017-11-07 16:28:59 -0200

    Updates

--- Recicla-Itape/recicla/app/Http/Kernel.php
+++ Recicla-Itape/recicla/app/Http/Kernel.php
@@ -64,10 +64,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-
-        'auth' => 'App\Http\Middleware\Authenticate',
-        'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-        'guest' => 'App\Http\Middleware\RedirectIfAuthenticated',
-    'admin' => 'App\Http\Middleware\Admin', // this line right here
 ];
 }
