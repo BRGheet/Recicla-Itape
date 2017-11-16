0f6849a4adc7881d4f41182c34caea1f9f620745
Miqueias08 <miqueiasfernando@gmail.com>
2017-11-16 17:59:55 -0200

    Organização Rotas

--- Recicla-Itape/recicla/routes/web.php
+++ Recicla-Itape/recicla/routes/web.php
@@ -7,33 +7,26 @@ Route::group(['namespace'=>'site'],function(){
 	Route::get('cooperativas', 'siteController@Cooperativas');
 	Route::get('tutoriais/{id}','siteController@Tutoriais');
 	Route::get('reciclar/f={dado}','siteController@mapFilter');
+});
 
 
 
+Route::group(['namespace'=>'Site\Auth'],function(){
+	$this->get('cadastro/login', 'LoginController@showLoginForm')->name('login');
+	$this->post('cadastro/login', 'LoginController@login')->name('login');
 
-	/*Login User*/
-	$this->get('cadastro/login', 'Auth\LoginController@showLoginForm')->name('login');
-	$this->post('cadastro/login', 'Auth\LoginController@login')->name('login');
+	$this->post('logout', 'LoginController@logout')->name('logout');
 
-	$this->post('logout', 'Auth\LoginController@logout')->name('logout');
-
-	Route::get('/minha_conta', 'User\HomeController@AlterarDados')->name('home');
-	Route::get('/vouchers', 'User\HomeController@Index')->name('home');
-
-	/*Rotas Registro De Usuarios*/
-	$this->get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
-	$this->post('register', 'Auth\RegisterController@register');
+	$this->get('register', 'RegisterController@showRegistrationForm')->name('register');
+	$this->post('register', 'RegisterController@register');
+});
+Route::group(['namespace'=>'Site\User'],function(){
+	Route::get('/minha_conta', 'HomeController@AlterarDados')->name('home');
+	Route::get('/vouchers', 'HomeController@Index')->name('home');
 });
 
 /*Rotas Da Interface Adminitrativa*/
-Route::group(['middleware' => 'CheckAdmin'], function (){
-	Route::group(['namespace'=>'Admin\Auth'],function(){
-		Route::get('admin/login', 'LoginAdminController@login');
-		Route::post('admin/login', 'LoginAdminController@loginValidation');
-			Route::get('admin/logout', 'LoginAdminController@logout');
-	});
-});
-Route::group(['middleware' => ['CheckAdmin']], function () {
+Route::group(['middleware' => ['CheckAdmin']],function () {
 Route::group(['namespace'=>'Admin'],function(){
 		Route::get('admin', 'adminController@admin');
 		Route::get('admin/index', 'adminController@index');
@@ -47,6 +40,7 @@ Route::group(['namespace'=>'Admin'],function(){
 		Route::post('admin/gift/send', 'adminController@giftStore');
 	});
 });
+
 // Resetar Senhas
 $this->get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
 $this->post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
